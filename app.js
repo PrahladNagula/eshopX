88160990fffa5a5374e5c2709af12f147c7e3752
Prahlad Nagula <prahladnagula222@gmail.com>
2023-09-10 21:46:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const path = require('path');
+
+const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
+
+const createSessionConfig = require('./config/session');
+const db = require('./data/database');
+//Importing all the middlewares 
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
+const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
+const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
+//Importing all the routes
+const authRoutes = require('./routes/auth.routes');
+const productsRoutes = require('./routes/products.routes');
+const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
+const ordersRoutes = require('./routes/orders.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+app.use('/products/assets', express.static('product-data'));
+app.use(express.urlencoded({ extended: false }));
+//This is for using the req.body
+app.use(express.json());
+
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(cartMiddleware);
+app.use(updateCartPricesMiddleware);
+
+app.use(addCsrfTokenMiddleware);
+// imp to use after app.use(csrf()) , bec we are using the csrfToken() func
+//app.use basically checks for all posts requests
+app.use(checkAuthStatusMiddleware);
+
+app.use(baseRoutes);
+app.use(authRoutes);
+//app.use() works as middleware and every request passes from the path defined in app.use()
+//it will be triggered for every incoming request
+app.use(productsRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
+
+app.use(errorHandlerMiddleware);
+//connectToDb is an async func thats why returns a promise, and we can use then catch in it
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
+    console.log(error);
+  });
